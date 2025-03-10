78008bee1e2128282c2e8e9979f2f2410f827e0e
ajitkumar1264 <vaghelaajit464@gmail.com>
2025-03-09 20:22:14 -0700

    added configuration of keyclock for Backend

--- HMS/src/config/AuthConfig.ts
+++ HMS/src/config/AuthConfig.ts
@@ -1,6 +1,6 @@
 export const oidcConfig = {
-  authority: "http://localhost:4000/realms/HMS",
-  client_id: "my-client-id",
+  authority: "http://localhost:4000/realms/myrealm",
+  client_id: "react-app",
   redirect_uri: "http://localhost:5173/home",
   post_logout_redirect_uri: "http://localhost:5173/login",
   onSigninCallback: () => {
