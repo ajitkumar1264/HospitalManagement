429f17cf2163fbcb4680ee95eac54aa25221aecb
ajitkumar1264 <vaghelaajit464@gmail.com>
2025-04-05 09:51:02 -0700

    solve error of eslint

--- HMS/src/pages/Protected.tsx
+++ HMS/src/pages/Protected.tsx
@@ -5,7 +5,7 @@ function Protected() {
 
   return (
     <div>
-      {/* <h1>Protected</h1>
+      <h1>Protected</h1>
 
       {loading && <p>Loading user rights...</p>}
       {error && <p style={{ color: 'red' }}>Error: {error}</p>}
@@ -14,7 +14,7 @@ function Protected() {
           <h3>API Response:</h3>
           <p>{data}</p>
         </div>
-      )} */}
+      )}
     </div>
   );
 }
